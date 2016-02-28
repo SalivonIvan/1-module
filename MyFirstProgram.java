a586c0cb4733e4546e45e32446e954b9394ba0d7
SalivonIvan <salivonlvan@gmail.com>
2016-02-28 23:44:30 +0200

    Update MyFirstProgram.java
    1. Класи повинні бути у пакетах!!!
    2. Форматуй код, він тоді краще читається, в кожній ІДЕ є свій набір клавіш для цього.

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,5 +1,7 @@
+package ua.goit.gojavaonline2.surname
+
 public class MyFirstProgram {
-public static void main (String[] args) {
-System.out.println ("Hi, I'm your first Java program! Stay tuned!");
-}
+    public static void main (String[] args) {
+       System.out.println ("Hi, I'm your first Java program! Stay tuned!");
+    }
 }
